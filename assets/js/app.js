05929765a6d720950b3fd84ebe36668058690113
Napster <rome.1205h@gmail.com>
2020-05-05 14:02:45 -0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+//Variables
+const listaTweets = document.getElementById('lista-tweets');
+
+// Event listeners
+
+eventListener();
+
+function eventListener() {
+    //cuando se envia al formulario
+    document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+}
+
+
+//Functions
+//Añadir tweet del formulario
+
+function agregarTweet(e) {
+    e.preventDefault();
+    // Leer valor de text area
+    const tweet = document.getElementById('tweet').value;
+    //Crear boton para eliminar
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'borrar-tweet';
+    botonBorrar.innerText = 'X';
+
+    //Crear elemento y añadirle el contenido a la lista
+    const li = document.createElement('li');
+    li.innerText = tweet;
+    //Añade el boton de borrar el tweet
+    li.appendChild(botonBorrar);
+    //Añade el tweet a la lista
+    listaTweets.appendChild(li);
+
+    console.log(li);
+}
+
