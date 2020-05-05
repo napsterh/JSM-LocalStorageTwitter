42f23246890a25fd4c9775247ba524b36cc5200d
Napster <rome.1205h@gmail.com>
2020-05-05 18:40:17 -0500

    Eliminar dato de localStorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,9 @@ function eventListener() {
 
     //Contenido cargado
     document.addEventListener('DOMContentLoaded', localStorageListo);
+
+    //Borrar  tweet
+    document.addEventListener('submit', borrarCampo);
 }
 
 
@@ -25,22 +28,21 @@ function agregarTweet(e) {
     e.preventDefault();
     // Leer valor de text area
     const tweet = document.getElementById('tweet').value;
-    //Crear boton para eliminar
-    const botonBorrar = document.createElement('a');
-    botonBorrar.classList = 'borrar-tweet';
-    botonBorrar.innerText = 'X';
+        //Crear boton para eliminar
+        const botonBorrar = document.createElement('a');
+        botonBorrar.classList = 'borrar-tweet';
+        botonBorrar.innerText = 'X';
 
-    //Crear elemento y añadirle el contenido a la lista
-    const li = document.createElement('li');
-    li.innerText = tweet;
-    //Añade el boton de borrar el tweet
-    li.appendChild(botonBorrar);
-    //Añade el tweet a la lista
-    listaTweets.appendChild(li);
+        //Crear elemento y añadirle el contenido a la lista
+        const li = document.createElement('li');
+        li.innerText = tweet;
+        //Añade el boton de borrar el tweet
+        li.appendChild(botonBorrar);
+        //Añade el tweet a la lista
+        listaTweets.appendChild(li);
 
-    //Añadir a local storage
-    agregarTweetLocalStorage(tweet);
-
+        //Añadir a local storage
+        agregarTweetLocalStorage(tweet);
 }
 
 //Elimina el tweet
@@ -48,8 +50,7 @@ function borrarTweet(e){
     e.preventDefault();
     if(e.target.className === 'borrar-tweet'){
         e.target.parentElement.remove();
-        //console.log(e.target.parentElement.remove());
-        alert('Tweet eliminado');
+        borrarTweetLocalStorage(e.target.parentElement.innerText);
     }
 }
 
@@ -81,13 +82,14 @@ function localStorageListo(){
 
 //Agrega tweet a LocalStorage
 function agregarTweetLocalStorage(tweet){
-    let tweets;
-    tweets = obtenerTweetsLocalStorage();
-    //Añadir el nuevo tweet
-    tweets.push(tweet);
-    //Convertir de string a arreglo para local storage
-    localStorage.setItem('tweets', JSON.stringify(tweets) );
-
+    if(tweet !== null){
+        let tweets;
+        tweets = obtenerTweetsLocalStorage();
+        //Añadir el nuevo tweet
+        tweets.push(tweet);
+        //Convertir de string a arreglo para local storage
+        localStorage.setItem('tweets', JSON.stringify(tweets) );
+    }
 }
 
 // Comprobar que existe elementos en localstorage, retorna un arreglo
@@ -100,4 +102,30 @@ function obtenerTweetsLocalStorage(){
         tweets = JSON.parse(localStorage.getItem('tweets'));
     }
     return tweets;
+}
+
+
+// Eliminar tweet de localStorage
+
+function borrarTweetLocalStorage(tweet){
+    let tweets, tweetBorrar;
+    // Elimina la X de tweet
+    tweetBorrar = tweet.substring(0, tweet.length - 1);
+
+    tweets = obtenerTweetsLocalStorage();
+
+    tweets.forEach(function(tweet, index){
+        if(tweetBorrar === tweet){
+            tweets.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+function borrarCampo(){
+    const campo = document.getElementById('tweet');
+    if(campo !== null){
+        campo.value = "";
+    }
 }
